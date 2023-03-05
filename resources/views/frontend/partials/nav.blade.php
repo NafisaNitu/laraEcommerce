24221ff5b3a9960ee0146090a936470a52a9e971
NafisaNitu <nafisanitu240@gmail.com>
2023-03-05 17:52:30 +0600

    add new doc

--- resources/views/frontend/partials/nav.blade.php
+++ resources/views/frontend/partials/nav.blade.php
@@ -16,6 +16,16 @@
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('contact') }}">Contact</a>
               </li>
+              <li class="nav-item">
+                <form class="form-inline my-2 my-lg-0" action="{{ route('search') }}" method="get">
+                  <div class="input-group">
+                      <input type="text" class="form-control" name="search" placeholder="Search products" aria-label="Recipient's username" aria-describedby="basic-addon2">
+                      <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="button"><i class="fa-solid fa-magnifying-glass"></i></button>
+                      </div>
+                    </div>
+                </form>
+              </li>
             {{-- <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 Dropdown
@@ -31,14 +41,27 @@
               <a class="nav-link disabled" href="#">Disabled</a>
             </li> --}}
           </ul>
-          <form class="form-inline my-2 my-lg-0" action="{{ route('search') }}" method="get">
-            <div class="input-group mb-3">
-                <input type="text" class="form-control" name="search" placeholder="Search products" aria-label="Recipient's username" aria-describedby="basic-addon2">
-                <div class="input-group-append">
-                  <button class="btn btn-outline-secondary" type="button"><i class="fa-solid fa-magnifying-glass"></i></button>
-                </div>
-              </div>
-          </form>
+          <div>
+            @if (Route::has('login'))
+            <ul class="navbar-nav ml-auto">
+              @auth
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url('/dashboard') }}">Dashboard</a>
+                </li>
+              @else
+
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('login') }}">Log in</a>
+                </li>
+              @if (Route::has('register'))
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('register') }}">Register</a>
+                </li>
+              @endif
+              @endauth
+            </ul>
+          @endif
+        </div>
         </div>
     </div>
 </nav>
